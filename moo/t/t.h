ac79225bb4eb5d4f992b54660238ec9b8aa8aeb9
hyunghwan.chung <hyunghwan.chung@c9d47f8d-4fed-4a2f-8cad-691f5fbe3904>
2019-05-03 06:49:57 +0000

    changed t/Makefile.am. enhanced the test macros

--- moo/t/t.h
+++ moo/t/t.h
@@ -3,8 +3,19 @@
 
 #include <stdio.h>
 
-#define T_ASSERT_FAIL1(msg1) printf("FAILURE in %s[%d] - %s\n", __func__, (int)__LINE__, msg1)
-#define T_ASSERT_FAIL2(msg1,msg2) printf("FAILURE in %s[%d] - %s - %s\n", __func__, (int)__LINE__, msg1, msg2)
+#if defined(__STDC_VERSION__) && (__STDC_VERSION__>=199901L)
+#	define T_ASSERT_FAIL1(msg1) printf("FAILURE in %s[%d] - %s\n", __func__,
+ (int)__LINE__, msg1)
+#	define T_ASSERT_FAIL2(msg1,msg2) printf("FAILURE in %s[%d] - %s - %s\n",
+ __func__, (int)__LINE__, msg1, msg2)
+#else
+#	define T_ASSERT_FAIL1(msg1) printf("FAILURE in %s[%d] - %s\n", __FILE__,
+ (int)__LINE__, msg1)
+#	define T_ASSERT_FAIL2(msg1,msg2) printf("FAILURE in %s[%d] - %s - %s\n",
+ __FILE__, (int)__LINE__, msg1, msg2)
+#endif
+
+
 
 #define T_ASSERT1(test,msg1) do { if (!(test)) { T_ASSERT_FAIL1(msg1); goto oops; } } while(0)
 #define T_ASSERT2(test,msg1,msg2) do { if (!(test)) { T_ASSERT_FAIL2(msg1,msg2); goto oops; } } while(0)
